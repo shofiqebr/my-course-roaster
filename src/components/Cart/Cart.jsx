468110c3dfa5800b1d7d9a000dc14d937f439e7e
shofiq <shofiqebr@gmail.com>
2023-09-16 01:52:36 +0600

    cart part added'

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,10 +1,25 @@
+/* eslint-disable no-unreachable */
+/* eslint-disable react/jsx-key */
+/* eslint-disable react/prop-types */
 
-const Cart = () => {
-    return (
-        <div className="w-72">
-            fklsdjkfksdgd
-        </div>
-    );
+const Cart = ({selectedCourse, totalHour, totalRemainingHour }) => {
+    console.log(selectedCourse);
+    return(
+         <div className="w-72 p-5">
+            <h2>   Credit Hour Remaining {totalRemainingHour} hr</h2>
+            <hr />
+            {
+                selectedCourse.map((course) => (
+                 <li key={course.id}>
+                    {course.title}
+                    </li>
+                ))
+            // eslint-disable-next-line react/jsx-no-comment-textnodes
+            }
+            <hr />
+            Total Credit Hour : {totalHour}
+         </div>
+     );
 };
 
 export default Cart;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,47 @@
 /* eslint-disable react/jsx-key */
 import { useEffect, useState } from 'react';
-import { BsBook } from 'react-icons/Bs';
+import { BsBook } from 'react-icons/bs';
 import { PiCurrencyDollar } from "react-icons/pi";
 import Cart from './../Cart/Cart';
 const Home = () => {
     const [allCourses, setAllCourses] = useState([]);
+    const [selectedCourse, setSelectedCourse] = useState([]);
+    const [totalHour, setTotalHour] = useState(0);
+    const [totalRemainingHour, setTotalRemainingHour] = useState(0);
         useEffect(() => {
             fetch("./data.json")
             .then((res) => res.json())
             .then((data) => setAllCourses(data));
         },[]);
+
+        const handleSelectCourse = (course) => {
+            const isExist = selectedCourse.find((item) => item.id == course.id);
+
+            let count = course.credit;
+            if(isExist){
+              return  alert("already included");
+            }
+            else{
+                selectedCourse.forEach((item)=>{
+                count = count + item.credit;
+            });
+
+            const totalRemaining = 20-count;
+
+            if(count>20){
+                return alert("no remaining credit") 
+            }
+            else{
+                setTotalHour(count);
+               setTotalRemainingHour(totalRemaining);
+            }
+           
+                setSelectedCourse([...selectedCourse, course]);
+            }
+            // console.log(isExist);
+           
+        };
+        console.log(selectedCourse)
     return (
         <div className="flex pt-3">
             <div className='grid grid-cols-3 gap-3 '>
@@ -21,18 +53,18 @@ const Home = () => {
     <img src={course.cover} alt="Shoes" className="rounded-xl mt-3 h-44 w-72" />
   </figure>
   <div className="card-body items-center text-center">
-    <h2 className="card-title">{course.title}
+    <h2 className="card-title font-bold text-[17px]">{course.title}
     </h2>
-    <p>{course.description}</p>
-    <div className='flex justify-center items-center
+    <p className='text-sm py-3'>{course.description}</p>
+    <div className='pb-3 flex justify-center items-center
      gap-3'>
       <div className="symbole"><PiCurrencyDollar></PiCurrencyDollar></div>
       <div>price : {course.price}</div>
       <div className="symbole"><BsBook></BsBook></div>
-      <div>Credit : {course.credit}</div>
+      <div>Credit : {course.credit}hr</div>
    </div>
     <div className="card-actions ">
-      <button className="btn btn-info w-72">Select</button>
+      <button onClick={()=> handleSelectCourse(course)} className="btn btn-info w-72">Select</button>
     </div>
   </div>
 </div>
@@ -45,7 +77,7 @@ const Home = () => {
 
 
             <div className="cart-container">
-                <Cart></Cart>
+                <Cart selectedCourse={selectedCourse} totalHour={totalHour} totalRemainingHour={totalRemainingHour}></Cart>
             </div>
 
 
